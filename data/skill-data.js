dfae78189c85db93fa3742c24d72b6e26cf63e5f
Megan Smith <megans766@gmail.com>
2023-01-04 20:14:57 -0500

    add data folder

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {skill: "JavaScript", learned: true, _id:267156},
+  {skill: "HTML", learned: true, _id:260986},
+  {skill: "CSS", learned: true, _id:271346}
+]
+
+export {
+  skills
+}
