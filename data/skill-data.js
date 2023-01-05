6ccaba7c393587275f250c2ffe8a16c8be2d1d12
Megan Smith <megans766@gmail.com>
2023-01-04 20:24:33 -0500

    update data and skills folders

--- data/skill-data.js
+++ data/skill-data.js
@@ -1,7 +1,10 @@
 const skills = [
   {skill: "JavaScript", learned: true, _id:267156},
   {skill: "HTML", learned: true, _id:260986},
-  {skill: "CSS", learned: true, _id:271346}
+  {skill: "CSS", learned: true, _id:271346},
+  {skill: "Node.js", learned: false, _id:271096},
+  {skill: "Express", learned: true, _id:171346},
+  {skill: "Mongoose", learned: true, _id:109856}
 ]
 
 export {
